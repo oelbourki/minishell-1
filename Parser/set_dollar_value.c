7c55a57f2fe4cdb1238bf2846a4eac0ab121f446
kirwa-KO <56740152+kirwa-KO@users.noreply.github.com>
2020-02-11 13:22:41 +0100

    hamdolah

---
+++ Parser/set_dollar_value.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_dollar_value.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-bour <oel-bour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/06 13:35:38 by ibaali            #+#    #+#             */
+/*   Updated: 2020/02/09 22:49:16 by oel-bour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+t_env	*g_var_tmp;
+
+void	set_g_variables(char *new_str, char *var, int *j)
+{
+	int		k;
+
+	g_var_tmp = g_variables;
+	while (g_var_tmp != NULL)
+	{
+		if (ft_strncmp(var, g_var_tmp->variable, ft_strlen(var)) == 0)
+		{
+			k = 0;
+			while (g_var_tmp->value[k] != '\0')
+				new_str[(*j)++] = g_var_tmp->value[k++];
+			new_str[*j] = '\0';
+			return ;
+		}
+		g_var_tmp = g_var_tmp->next;
+	}
+}
+
+char	*set_command_return(char *new_str)
+{
+	int		k;
+	char	*ret_string;
+
+	k = 0;
+	ret_string = ft_itoa(g_status);
+	ft_strcpy(new_str, ret_string);
+	return (new_str);
+}
+
+void	set_g_environt(char *new_str, char *var)
+{
+	int		k;
+	int		j;
+
+	j = 0;
+	g_var_tmp = g_environt;
+	while (g_var_tmp != NULL)
+	{
+		if (ft_strncmp(var, g_var_tmp->variable, ft_strlen(var)) == 0)
+		{
+			k = 0;
+			while (g_var_tmp->value[k] != '\0')
+				new_str[j++] = g_var_tmp->value[k++];
+			new_str[j] = '\0';
+			return ;
+		}
+		g_var_tmp = g_var_tmp->next;
+	}
+	set_g_variables(new_str, var, &j);
+	new_str[j] = '\0';
+}
+
+char	*put_value_of_dollar(char *str, int *i)
+{
+	char	var[256];
+	char	*new_str;
+	int		p;
+
+	p = 0;
+	if ((new_str = malloc(4096)) == NULL)
+		return (NULL);
+	new_str[0] = '\0';
+	*i += 1;
+	while (ft_isalnum(str[*i]) == 1 || str[*i] == '_' || str[*i] == '?')
+		var[p++] = str[(*i)++];
+	var[p] = '\0';
+	if (strcmp(var, "?") == 0)
+	{
+		*i -= 1;
+		return (set_command_return(new_str));
+	}
+	*i -= 1;
+	set_g_environt(new_str, var);
+	return (new_str);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int		count;
+
+	count = 0;
+	while (src[count] != '\0')
+	{
+		dest[count] = src[count];
+		count++;
+	}
+	dest[count] = '\0';
+	return (dest);
+}
---
+++ execution/redirections.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-bour <oel-bour@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/04 11:23:32 by oel-bour          #+#    #+#             */
+/*   Updated: 2020/02/09 22:16:35 by oel-bour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+int		cmd(t_command **head)
+{
+	g_counter = *head;
+	if (g_counter != NULL && g_counter->what == STRING)
+	{
+		g_counter->what = COMMAND;
+		help_convert1(g_counter);
+		return (1);
+	}
+	else
+		return (0);
+}
+
+int		help_do_somein(void)
+{
+	if (g_counter->next->what != REDIN && g_counter->next->what != REDOUT &&
+	g_counter->next->what != DOUBLEREDOUT && g_counter->next->str != NULL)
+	{
+		g_in_fd = open(g_counter->next->str, O_RDONLY, 0);
+		g_counter->next->what = STRING;
+		if (g_counter->next->next != NULL &&
+		g_counter->next->next->what == REDIN)
+			close(g_in_fd);
+		else if (g_in_fd < 0)
+			return (error());
+	}
+	else if (g_counter->next->what == REDIN)
+		ft_print("bash: syntax error near unexpected token `<'\n");
+	else
+		return (-1);
+	return (0);
+}
+
+int		do_somein(t_command **head)
+{
+	g_counter = *head;
+	while (g_counter != NULL && g_counter->what == REDIN)
+	{
+		if (g_counter->next == NULL)
+		{
+			ft_print("bash: syntax error near unexpected token `newline\'\n");
+			return (-1);
+		}
+		if (help_do_somein() == -1)
+			return (-1);
+		g_counter = g_counter->next->next;
+	}
+	if (g_counter != NULL && (g_counter->what == REDOUT ||
+	g_counter->what == DOUBLEREDOUT) && do_someout(&g_counter))
+		return (1);
+	g_mul_redin = 1;
+	return (cmd(&g_counter));
+}
+
+int		help_do_someout(void)
+{
+	if (g_counter->next->what != REDOUT && g_counter->next->what !=
+	DOUBLEREDOUT && g_counter->next->str != NULL)
+	{
+		if (g_counter->what == REDOUT)
+			g_out_fd = open(g_counter->next->str,
+			O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		else
+			g_out_fd = open(g_counter->next->str, O_RDWR
+			| (O_APPEND | O_CREAT), S_IRWXU);
+		g_counter->next->what = STRING;
+		if (g_counter->next->next != NULL && (g_counter->next->next->what
+		== REDOUT || g_counter->next->next->what == DOUBLEREDOUT))
+			close(g_out_fd);
+		else if (g_in_fd < 0)
+			return (error());
+	}
+	else if (g_counter->next->what == REDOUT)
+		ft_print("bash: syntax error near unexpected token `>'\n");
+	else if (g_counter->next->what == DOUBLEREDOUT)
+		ft_print("bash: syntax error near unexpected token `>>'\n");
+	else
+		return (-1);
+	return (0);
+}
+
+int		do_someout(t_command **head)
+{
+	g_counter = *head;
+	while (g_counter != NULL && (g_counter->what == REDOUT ||
+	g_counter->what == DOUBLEREDOUT))
+	{
+		if (g_counter->next == NULL)
+		{
+			ft_print("bash: syntax error near unexpected token `newline\'\n");
+			return (-1);
+		}
+		if (help_do_someout() == -1)
+			return (-1);
+		g_counter = g_counter->next->next;
+	}
+	if (g_counter != NULL && g_counter->what == REDIN && do_somein(&g_counter))
+		return (1);
+	g_multi_redout = 1;
+	return (cmd(&g_counter));
+}
